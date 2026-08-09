94c2328cdd47f5b06c58efeb54ad4f575c9acdbd
Eduard Petrov <yousikey@gmail.com>
2026-08-09 21:09:26 +1200

    Update table.js

--- src/components/table.js
+++ src/components/table.js
@@ -11,13 +11,44 @@ export function initTable(settings, onAction) {
     const {tableTemplate, rowTemplate, before, after} = settings;
     const root = cloneTemplate(tableTemplate);
 
-    // @todo: #1.2 —  вывести дополнительные шаблоны до и после таблицы
+    // #1.2 — вывести дополнительные шаблоны до и после таблицы
+    before.reverse().forEach(subName => {           // перебираем идентификаторы шаблонов в обратном порядке
+        root[subName] = cloneTemplate(subName);     // клонируем и сохраняем в объекте таблицы
+        root.container.prepend(root[subName].container);    // добавляем до таблицы
+    });
 
-    // @todo: #1.3 —  обработать события и вызвать onAction()
+    after.forEach(subName => {
+        root[subName] = cloneTemplate(subName);
+        root.container.append(root[subName].container);     // добавляем после таблицы
+    });
+
+    // #1.3 — обработать события и вызвать onAction()
+    root.container.addEventListener('change', () => {
+        onAction();
+    });
+
+    root.container.addEventListener('reset', () => {
+        setTimeout(onAction);       // поля очищаются позже самого события reset
+    });
+
+    root.container.addEventListener('submit', (e) => {
+        e.preventDefault();
+        onAction(e.submitter);      // передаём нажатую кнопку
+    });
 
     const render = (data) => {
-        // @todo: #1.1 — преобразовать данные в массив строк на основе шаблона rowTemplate
-        const nextRows = [];
+        // #1.1 — преобразовать данные в массив строк на основе шаблона rowTemplate
+        const nextRows = data.map(item => {
+            const row = cloneTemplate(rowTemplate);
+
+            Object.keys(item).forEach(key => {
+                if (key in row.elements) {
+                    row.elements[key].textContent = item[key];
+                }
+            });
+
+            return row.container;
+        });
         root.elements.rows.replaceChildren(...nextRows);
     }
 
