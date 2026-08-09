f180b6238820c8484fcdd888154bc9751d9b872a
Eduard Petrov <yousikey@gmail.com>
2026-08-09 21:08:53 +1200

    Update sorting.js

--- src/components/sorting.js
+++ src/components/sorting.js
@@ -1,18 +1,34 @@
-import {sortCollection, sortMap} from "../lib/sort.js";
+import {sortMap} from "../lib/sort.js";
 
 export function initSorting(columns) {
-    return (data, state, action) => {
+    return (query, state, action) => {
         let field = null;
         let order = null;
 
         if (action && action.name === 'sort') {
-            // @todo: #3.1 — запомнить выбранный режим сортировки
+            // #3.1 — запомнить выбранный режим сортировки
+            action.dataset.value = sortMap[action.dataset.value];   // следующее состояние из карты переходов
+            field = action.dataset.field;                           // поле, по которому сортируем
+            order = action.dataset.value;                           // направление сортировки
 
-            // @todo: #3.2 — сбросить сортировки остальных колонок
+            // #3.2 — сбросить сортировки остальных колонок
+            columns.forEach(column => {
+                if (column.dataset.field !== action.dataset.field) {
+                    column.dataset.value = 'none';
+                }
+            });
         } else {
-            // @todo: #3.3 — получить выбранный режим сортировки
+            // #3.3 — получить выбранный режим сортировки
+            columns.forEach(column => {
+                if (column.dataset.value !== 'none') {
+                    field = column.dataset.field;
+                    order = column.dataset.value;
+                }
+            });
         }
 
-        return sortCollection(data, field, order);
+        const sort = (field && order !== 'none') ? `${field}:${order}` : null;  // параметр сортировки в виде field:direction
+
+        return sort ? Object.assign({}, query, {sort}) : query;   // если есть сортировка — добавляем, иначе не трогаем query
     }
 }
